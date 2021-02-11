dae63c0cca95db8a6f4f63ecf1daf8153b9aadeb
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-02-11 15:39:40 -0700

    added instructions_msg

--- lib/messages.rb
+++ lib/messages.rb
@@ -7,6 +7,8 @@ class Messages
   end
 
   def instructions_msg
-    print "The computer will generate a sequence of four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow\n\nThe objective of the game is to figure out the sequence in the correct order.\n\nAs you make guesses the computer will provide you with feedback describing:\n\n\nThe number of correct elements included in the guess\n\nAND\n\nThe number of correct positions\n"
+    require "pry"; binding.pry
+    print "\n\n\nThe computer will generate a sequence of four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow\nThe objective of the game is to figure out the sequence in the correct order.\nAs you make guesses the computer will provide you with feedback describing:\n\n\nThe number of correct elements in your guess\n\nAND\n\nThe number of correct positions in your guess\n\n\nWhen you guess the computer generated sequence you will be notified with the following message:\n\n\nCongratulations! You have guessed the sequence '_ _ _ _' in _# guesses over _ minutes, _ seconds.\nDo you want to (p)lay again or (q)uit\n\n\nGood Luck!"
+
   end
 end
