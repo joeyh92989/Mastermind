a9f85277702be58a296af519c2b123b115602107
Joey Haas <josephthaas@gmail.com>
2021-02-13 14:17:50 -0700

    created correct position method

---
+++ lib/code_evaluator.rb
@@ -0,0 +1,16 @@
+class CodeEvaluator
+  attr_reader :code, :code_string
+
+  def initialize(code, code_string)
+    @code = code
+    @code_string = code_string
+  end
+
+  def correct_position(guess)
+    # code takes in an argument of an array and responds with a intenger of the number of correct postions between the two arrays for purposes of providing a response for messsages
+
+    #
+    pairs= @code.zip(guess)
+    correct_position_count = pairs.select { |pair| pair[0] == pair[1]}.count
+  end
+end
--- test/test_code_evaluator.rb
+++ test/test_code_evaluator.rb
@@ -1,3 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/nyan_cat'
+require './lib/game'
+require './lib/code_evaluator'
+
+class CodeEvaluatorTest < MiniTest::Test
+
+  def test_it_exist
+    code_evaluator = CodeEvaluator.new(['r','b','y','g'], "rbyg")
+
+    assert_instance_of CodeEvaluator, code_evaluator
+  end
+
+  def test_it_exist_with_attributes
+    code_evaluator = CodeEvaluator.new(['r','b','y','g'], "rbyg")
+
+    assert_equal ['r','b','y','g'], code_evaluator.code
+    assert_equal "rbyg", code_evaluator.code_string
+  end
+
+  def test_correct_position
+    code_evaluator = CodeEvaluator.new(['r','b','y','g'], "rbyg")
+    correct_position = ['r','b','y','b']
+    
+    assert_equal 3, code_evaluator.correct_position(correct_position)
+  end
+end
+
+
+
+
+
+
 # # psuedo code
 #
 # its a class
