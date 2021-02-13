3355406ae893e42161701f7b655823de8ed87bba
Joey Haas <josephthaas@gmail.com>
2021-02-13 13:07:02 -0700

    added pseudo code to test code eval

---
+++ test/test_code_evaluator.rb
@@ -0,0 +1,21 @@
+# # psuedo code
+#
+# its a class
+#
+# initialize (code,code_string)
+# Attributes
+# - Code
+# - Code String
+#
+#
+# giving_feedback_for_incorrect_guesses (guess)
+# * this must return two integers
+#
+#  the first integer must indicate guess compare colors independent of index and disregarding correct indexes
+#
+#  the second integer must indicate guess compare indexes to store code attribute
+#
+#
+# helper_method_to_get_first_integer
+#
+# helper_method_to_get_second_integer
